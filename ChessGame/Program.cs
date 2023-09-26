16b33a2aed89dad972cc6dd788719e7081e326a9
Lucas Toledo <toledo.lucas06@gmail.com>
2023-09-26 11:15:56 -0300

    Criada a classe Posição

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChessGame.Tabuleiro;
+using System;
+
+namespace ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            
+        }
+    }
+}
