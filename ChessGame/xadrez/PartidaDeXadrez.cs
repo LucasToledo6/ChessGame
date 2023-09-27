c619178e849b3263b58f867c04f04831196030dd
Lucas Toledo <toledo.lucas06@gmail.com>
2023-09-27 11:23:29 -0300

    Correção: mudando o nome do método em Peca para movimentoPossivel

--- ChessGame/xadrez/PartidaDeXadrez.cs
+++ ChessGame/xadrez/PartidaDeXadrez.cs
@@ -96,7 +96,7 @@ namespace ChessGame.xadrez
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!Tab.Peca(origem).podeMoverPara(destino))
+            if (!Tab.Peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
